f5860ee44f209e5c69fb6086c8a612f32219ba63
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2016-05-02 11:26:10 +0100

    Added project used to generate FLAMEGPU model initialisation files.

---
+++ single machine/FLAMEGPU/FLAME Circles Init Gen/Debug/Gen Scaling.bat
@@ -0,0 +1,32 @@
+"FLAME Circles Init Gen.exe" -w 50 -c 1250 -f 50.xml
+"FLAME Circles Init Gen.exe" -w 60 -c 2160 -f 60.xml
+"FLAME Circles Init Gen.exe" -w 70 -c 3430 -f 70.xml
+"FLAME Circles Init Gen.exe" -w 80 -c 5120 -f 80.xml
+"FLAME Circles Init Gen.exe" -w 90 -c 7290 -f 90.xml
+"FLAME Circles Init Gen.exe" -w 100 -c 10000 -f 100.xml
+"FLAME Circles Init Gen.exe" -w 110 -c 13310 -f 110.xml
+"FLAME Circles Init Gen.exe" -w 120 -c 17280 -f 120.xml
+"FLAME Circles Init Gen.exe" -w 130 -c 21970 -f 130.xml
+"FLAME Circles Init Gen.exe" -w 140 -c 27440 -f 140.xml
+"FLAME Circles Init Gen.exe" -w 150 -c 33750 -f 150.xml
+"FLAME Circles Init Gen.exe" -w 160 -c 40960 -f 160.xml
+"FLAME Circles Init Gen.exe" -w 170 -c 49130 -f 170.xml
+"FLAME Circles Init Gen.exe" -w 180 -c 58320 -f 180.xml
+"FLAME Circles Init Gen.exe" -w 190 -c 68590 -f 190.xml
+"FLAME Circles Init Gen.exe" -w 200 -c 80000 -f 200.xml
+"FLAME Circles Init Gen.exe" -w 210 -c 92610 -f 210.xml
+"FLAME Circles Init Gen.exe" -w 220 -c 106480 -f 220.xml
+"FLAME Circles Init Gen.exe" -w 230 -c 121670 -f 230.xml
+"FLAME Circles Init Gen.exe" -w 240 -c 138240 -f 240.xml
+"FLAME Circles Init Gen.exe" -w 250 -c 156250 -f 250.xml
+"FLAME Circles Init Gen.exe" -w 260 -c 175760 -f 260.xml
+"FLAME Circles Init Gen.exe" -w 270 -c 196830 -f 270.xml
+"FLAME Circles Init Gen.exe" -w 280 -c 219520 -f 280.xml
+"FLAME Circles Init Gen.exe" -w 290 -c 243890 -f 290.xml
+"FLAME Circles Init Gen.exe" -w 300 -c 270000 -f 300.xml
+
+REM SET DENSITY = 0.01
+REM FOR /L %%A IN (50,10,300) DO (
+REM SET /A RESULT = %%A * %%A * %%A * DENSITY
+REM "FLAME Circles Init Gen.exe" -w %%A -c 270000 -f %%A.xml
+REM )
---
+++ single machine/FLAMEGPU/FLAME Circles Init Gen/FLAME Circles Init Gen/main.cpp
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <fstream>
+
+#include "rapidxml.hpp"
+#include "rapidxml_print.hpp"
+
+int main(int argc, char**argv)
+{
+	if (argc<6)
+	{
+		printf("<executable> -w WIDTH -c AGENT_COUNT -f OUTPUT_FILE\n");
+		exit(1);
+	}
+	unsigned int width = 0;
+	unsigned int count = 0;
+	std::string outFile = std::string("");
+	for (int i = 0; i < argc; i++)
+	{
+		std::string arg(argv[i]);
+		std::transform(arg.begin(), arg.end(), arg.begin(), ::tolower);
+		if (arg.compare("-w") == 0)
+		{
+			width = (unsigned int)strtoul(argv[++i], nullptr, 0);
+		}
+		else if (arg.compare("-c") == 0)
+		{
+			count = (unsigned int)strtoul(argv[++i], nullptr, 0);
+		}else if (arg.compare("-f") == 0)
+		{
+			outFile = std::string(argv[++i]);
+		}
+	}
+	if (width == 0)
+	{
+		printf("Err: Width must be >0\n");
+		exit(1);
+	}
+	if (count == 0)
+	{
+		printf("Err: Agent count must be >0\n");
+		exit(1);
+	}
+	if (outFile.empty())
+	{
+		printf("Err: Output file name must be specified\n");
+		exit(1);
+	}
+	//Actually do the generation
+	rapidxml::xml_document<char> doc;
+	//Common strings
+	char *states_node_str = doc.allocate_string("states");
+		char *itno_node_str = doc.allocate_string("itno");
+		char *xagent_node_str = doc.allocate_string("xagent");
+			char *name_node_str = doc.allocate_string("name");
+			char *id_node_str = doc.allocate_string("id");
+			char *x_node_str = doc.allocate_string("x");
+			char *y_node_str = doc.allocate_string("y");
+			char *z_node_str = doc.allocate_string("z");
+			char *fx_node_str = doc.allocate_string("fx");
+			char *fy_node_str = doc.allocate_string("fy");
+			char *fz_node_str = doc.allocate_string("fz");
+	char *circle_str = doc.allocate_string("Circle");
+	char *zero_pt_zero_str = doc.allocate_string("0.0");
+	//temp stuff
+	char buffer[1024];
+	std::mt19937 rng((unsigned int)std::chrono::system_clock::now().time_since_epoch().count());  // mt19937 is a standard mersenne_twister_engine
+	std::uniform_real_distribution<float> rng_float(0.0, width);
+	//states
+	rapidxml::xml_node<> *states_node = doc.allocate_node(rapidxml::node_element, states_node_str);
+	doc.append_node(states_node);
+	//itno
+	rapidxml::xml_node<> *itno_node = doc.allocate_node(rapidxml::node_element, itno_node_str, doc.allocate_string("0"));
+	states_node->append_node(itno_node);
+	//xagent each
+	for (unsigned int i = 0; i < count;i++)
+	{
+		rapidxml::xml_node<> *xagent_node = doc.allocate_node(rapidxml::node_element, xagent_node_str);
+		{
+			rapidxml::xml_node<> *name_node = doc.allocate_node(rapidxml::node_element, name_node_str, circle_str);
+			xagent_node->append_node(name_node);
+
+			sprintf(buffer, "%d", i);
+			rapidxml::xml_node<> *id_node = doc.allocate_node(rapidxml::node_element, id_node_str, doc.allocate_string(buffer));
+			xagent_node->append_node(id_node);
+
+			sprintf(buffer, "%f", rng_float(rng));
+			rapidxml::xml_node<> *x_node = doc.allocate_node(rapidxml::node_element, x_node_str, doc.allocate_string(buffer));
+			xagent_node->append_node(x_node);
+
+			sprintf(buffer, "%f", rng_float(rng));
+			rapidxml::xml_node<> *y_node = doc.allocate_node(rapidxml::node_element, y_node_str, doc.allocate_string(buffer));
+			xagent_node->append_node(y_node);
+
+			sprintf(buffer, "%f", rng_float(rng));
+			rapidxml::xml_node<> *z_node = doc.allocate_node(rapidxml::node_element, z_node_str, doc.allocate_string(buffer));
+			xagent_node->append_node(z_node);
+
+			rapidxml::xml_node<> *fx_node = doc.allocate_node(rapidxml::node_element, fx_node_str, zero_pt_zero_str);
+			xagent_node->append_node(fx_node);
+
+			rapidxml::xml_node<> *fy_node = doc.allocate_node(rapidxml::node_element, fy_node_str, zero_pt_zero_str);
+			xagent_node->append_node(fy_node);
+
+			rapidxml::xml_node<> *fz_node = doc.allocate_node(rapidxml::node_element, fz_node_str, zero_pt_zero_str);
+			xagent_node->append_node(fz_node);
+		}
+		states_node->append_node(xagent_node);
+	}
+	
+	//Actually do the output
+	std::ofstream f;
+	f.open(outFile);
+
+	f << doc;
+
+	f.close();
+}
